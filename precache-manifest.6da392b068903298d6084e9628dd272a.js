f3400680cd71a24ac662fbdee1ce02ea898bdf0f
Joao <55155875+potatosenior@users.noreply.github.com>
2020-06-02 21:38:59 -0300

    Updates

---
+++ precache-manifest.6da392b068903298d6084e9628dd272a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e97ef5f1066873743c9d08b2b1766f9",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "bf2469666a08b1916967",
+    "url": "/me/static/css/main.6242e283.chunk.css"
+  },
+  {
+    "revision": "c493cd515c737f7d5d52",
+    "url": "/me/static/js/2.d5eea7cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.d5eea7cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2469666a08b1916967",
+    "url": "/me/static/js/main.6dabd283.chunk.js"
+  },
+  {
+    "revision": "c9635c1d6c3dc912b64c",
+    "url": "/me/static/js/runtime-main.0d23ab0a.js"
+  }
+]);

082803262158aab75220aa754c9f5ca808bd53cb
Joao <55155875+potatosenior@users.noreply.github.com>
2020-06-04 20:48:25 -0300

    versao 1.0

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, {useState} from 'react';
 
 import './App.css';
 import theme from './utils/theme';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+import MainContent from './components/MainContent/MainContent';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import ChangeTheme from './components/ChangeTheme';
 
 function App() {
-  const [themeAtual, setTheme] = useState('light');
+  const [themeAtual, setTheme] = useState('dark');
 
   return (
     <div style={{backgroundColor: theme[themeAtual].third }} >
-      <Navbar theme={themeAtual} setTheme={setTheme} />
+      <Navbar theme={themeAtual} />
       <MainContent theme={themeAtual} />
       <Footer theme={themeAtual} />
+      <ChangeTheme theme={themeAtual} setTheme={setTheme} />
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,34 +1,30 @@
 import React from 'react';
 
-import theme from '../utils/theme'
+import theme from '../../utils/theme'
 
 function Footer(props) {
 
   const liStyle = Object.assign({}, styles.li)
   return (
-    <div style={{...styles.container, backgroundColor: theme[props.theme].third}} >
-      <p style={styles.p} >© MIT</p>
+    <div style={{...styles.container, backgroundColor: theme[props.theme].fourth}} >
+      <p style={styles.p} >© MIT - <span style={{color: '#000'}}>João Pedro</span></p>
+      
       <ul style={styles.ul} >
         <li style={liStyle} >
-          <a style={styles.a} href="https://www.github.com/potatosenior">
-            github
+          <a style={styles.a} href="https://www.github.com/potatosenior/Me">
+            Código fonte
           </a>
         </li>
-        <li style={liStyle}>
-          <a style={styles.a} href="https://www.linkedin.com/in/joãopedrovieira/">
-            linkedin
-          </a>
-        </li>
-        <li style={liStyle}>
+{/*         <li style={liStyle}>
           <a style={styles.a} href="https://play.google.com/store/apps/developer?id=Potatosenior">
-            plasytore
+            Plasytore
           </a>
         </li>
         <li style={liStyle}>
           <a style={styles.a} href="https://api.whatsapp.com/send?phone=5564992432941&text=Olá&source=&data=&app_absent=">
-            contato
+            Contato
           </a>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
@@ -39,9 +35,11 @@ const styles = {
     display: 'flex', 
     flexDirection: 'column', 
     color: 'white',
-    position: 'absolute',
+    /* position: 'absolute', */
     bottom: 0,
-    width: '100%'
+    width: '100%',
+    paddingTop: '4rem',
+    boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
   },
   p: {
     alignSelf: 'center'
@@ -63,7 +61,8 @@ const styles = {
   },
   a: {
     textDecoration: 'none',
-    color: 'white'
+    color: 'white',
+    fontSize: 'thin'
   }
 }
 
